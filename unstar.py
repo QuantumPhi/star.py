557625bc7768e79e5ade40ccbcd09879c638d1d9
Tarik Onalan <phi.quantum@gmail.com>
2015-01-14 22:49:35 -0800

    ...

--- unstar.py
+++ unstar.py
@@ -1,6 +1,11 @@
 import requests, json
 
 def authorize(user, pass):
-    payload = { "" }
+    payload = { }
     header = { "content-type": "application/vnd.github.mirage-preview+json" }
-    r = requests.get("https://api.github.com/authorizations", )
+    r = requests.get("https://api.github.com/authorizations", """something""")
+
+def toggle(user, repo):
+    payload = { }
+    header = { }
+    r = requests.get()
