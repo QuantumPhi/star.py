7c4b4118790c08dcf1476dfbcbe0ab10618277b1
Tarik Onalan <phi.quantum@gmail.com>
2015-02-04 08:41:51 -0800

    2FA codes not being sent

--- unstar.py
+++ unstar.py
@@ -3,15 +3,20 @@ from getpass import getpass
 from os.path import expanduser
 from os.path import isfile
 
-def authorize(user, pass):
-    payload = { }
-    r = requests.get("https://api.github.com/authorizations", """something""")
-    if(!isfile(expanduser("~/.unstar"))):
-        f = open(expanduser("~/.unstar/auth_token"))
-
-    header = { "content-type": "application/vnd.github.mirage-preview+json" }
+def authorize():
+    if(not(isfile(expanduser("~/.unstar")))):
+        user = raw_input("Username: ")
+        pswd = getpass("Password: ")
+        headers = { "content-type": "application/vnd.github.mirage-preview+json" }
+        payload = { "note": "star and unstar repositories from CLI" }
+        r = requests.get("https://api.github.com/authorizations", auth=(user, pswd), headers = headers, data=json.dumps(payload))
+        if(r.status_code == 401) and ("X-GitHub-OTP" in r.headers):
+            print(r.headers.get("X-GitHub-OTP"))
+        # f = open(expanduser("~/.unstar/auth_token"))
 
 def toggle(user, repo):
     payload = { }
     header = { }
     r = requests.get()
+
+authorize()
